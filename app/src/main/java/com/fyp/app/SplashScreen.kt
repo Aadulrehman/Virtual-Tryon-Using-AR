b50306092d4f7ca5f30853f869094c885decca68
abdul_rehman <itzabdulrehman010@gmail.com>
2023-06-17 15:51:34 +0500

    Gradle and Firebase
    project gradle upgrade,
    gradle wrapper upgrade
    firebase connected

--- app/src/main/java/com/fyp/app/SplashScreen.kt
+++ app/src/main/java/com/fyp/app/SplashScreen.kt
@@ -1,11 +1,26 @@
 package com.fyp.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
