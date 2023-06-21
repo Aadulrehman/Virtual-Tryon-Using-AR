fbd7ec0cf290f69c91202775b8941d3ddb506d28
abdul_rehman <itzabdulrehman010@gmail.com>
2023-06-21 21:12:52 +0500

    Bottom Navigation
    Added bottom navigation in fav
    Added Animation
    Added shifting of Activities

--- app/src/main/java/com/fyp/app/FavouriteActivity.kt
+++ app/src/main/java/com/fyp/app/FavouriteActivity.kt
@@ -1,11 +1,35 @@
 package com.fyp.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class FavouriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.BottomNavigation)
+        bottomNavigationView.selectedItemId = R.id.fav
+
+        bottomNavigationView.setOnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.fav -> return@setOnItemSelectedListener true
+                R.id.camera -> {
+                    startActivity(Intent(applicationContext, GlassesActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.home-> {
+                    startActivity(Intent(applicationContext, InventoryActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
--- app/src/main/java/com/fyp/app/InventoryActivity.kt
+++ app/src/main/java/com/fyp/app/InventoryActivity.kt
@@ -1,11 +1,36 @@
 package com.fyp.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class InventoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.BottomNavigation)
+        bottomNavigationView.selectedItemId = R.id.home
+
+        bottomNavigationView.setOnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.home -> return@setOnItemSelectedListener true
+                R.id.camera -> {
+                    startActivity(Intent(applicationContext, GlassesActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fav-> {
+                    startActivity(Intent(applicationContext, FavouriteActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
