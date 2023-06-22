0ce80d693e2acf1adab0a53e723673c81fa89c1a
abdul_rehman <itzabdulrehman010@gmail.com>
2023-06-22 21:49:56 +0500

    Recycler View
    Added RecyclerView

---
+++ app/src/main/java/com/fyp/app/DataClass.kt
@@ -0,0 +1,3 @@
+package com.fyp.app
+
+data class DataClass(var dataImage:Int, var dataTitle:String)
--- app/src/main/java/com/fyp/app/InventoryActivity.kt
+++ app/src/main/java/com/fyp/app/InventoryActivity.kt
@@ -4,13 +4,49 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class InventoryActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var dataList: ArrayList<DataClass>
+    lateinit var imageList:Array<Int>
+    lateinit var titleList:Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory)
+
+
+        imageList = arrayOf(
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash,
+            R.drawable.splash)
+
+        titleList = arrayOf(
+            "Glasses 1",
+            "Glasses 2",
+            "Glasses 3",
+            "Glasses 4",
+            "Glasses 5",
+            "Glasses 6",
+            "Glasses 7",
+            "Glasses 8")
+
+        recyclerView = findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        dataList = arrayListOf<DataClass>()
+        getData()
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.BottomNavigation)
         bottomNavigationView.selectedItemId = R.id.home
 
@@ -32,5 +68,13 @@ class InventoryActivity : AppCompatActivity() {
             }
             false
         }
+
+    }
+    private fun getData(){
+        for (i in imageList.indices){
+            val dataClass = DataClass(imageList[i], titleList[i])
+            dataList.add(dataClass)
+        }
+        recyclerView.adapter = AdapterClass(dataList)
     }
 }
